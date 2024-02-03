03a54435f345e46cbc54fde6fd94d529762f7b95
omaressaouaf <souafomar@gmail.com>
2024-02-03 13:06:55 +0100

    add nav active class

--- shared/nav.php
+++ shared/nav.php
@@ -1,4 +1,4 @@
 <nav>
-    <a href="index.php">Product List</a>
-    <a href="cart.php">Shopping Cart</a>
+    <a href="index.php" <?php echo (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'class="active"' : ''; ?>>Product List</a>
+    <a href="cart.php" <?php echo (basename($_SERVER['PHP_SELF']) == 'cart.php') ? 'class="active"' : ''; ?>>Shopping Cart</a>
 </nav>
